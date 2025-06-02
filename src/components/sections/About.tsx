e6c2c2e8b6c2a355776a943373ad542af83f8ba8
RajKKapadia <raajforyou@gmail.com>
2025-06-02 17:02:46 +0530

    updated about me

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -3,9 +3,7 @@ export function About() {
         <section className="container mx-auto px-4 py-20">
             <h2 className="text-3xl font-bold mb-8">About Me</h2>
             <div className="prose dark:prose-invert max-w-none">
-                <p className="text-justify">Team Lead Chat-bot Development at Let’s Enkindle with 6+ years of IT industry and freelance experience in chat-bot development on <strong>Google Dialogflow ES/CX</strong>, Large Language Model fine tuning, deployment, and utilization, API development with <strong>Python+Flask</strong> and
-                    <strong>NodeJS+Express</strong>, AI/ML/DL project development in the field of Image classification, Object detection, Natural Language Processing, search engine development using Elastic/Qdrant, part-time
-                    Youtuber.</p>
+                <p className="text-justify">Full time freelancer with 7+ years of IT industry and freelance experience in chat-bot development on <strong>Google Dialogflow ES/CX</strong>, Large Language Model fine tuning, deployment, and utilization, API development with <strong>Python+(Flask/FastAPI)</strong> and <strong>NodeJS+Express</strong>, AI/ML/DL project development in the field of Image classification, Object detection, Natural Language Processing, search engine development using Elastic/Qdrant, part-time Youtuber.</p>
             </div>
         </section>
     )
